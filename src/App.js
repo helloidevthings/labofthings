1fcd0f134baf7ddd95a083e4a5f407785ff4591f
Joyanna <helloidevthings@gmail.com>
2021-02-09 22:07:52 -0500

    changes to home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
 import theme from "./theme";
@@ -13,20 +12,7 @@ function App() {
       <AppStyles>
         <div className="App">
           <header className="App-header">
-            <img
-              src={logo}
-              className="App-logo"
-              alt="logo"
-            />
             <p>My new app </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
           </header>
         </div>
       </AppStyles>
